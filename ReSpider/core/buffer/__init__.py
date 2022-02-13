76d7cfb6924357a97cc4a09b113a55b2a8da7fd4
zhaoxiangpeng <1943364377@qq.com>
2022-02-13 22:58:39 +0800

    item_buffer demo

--- ReSpider/core/buffer/__init__.py
+++ ReSpider/core/buffer/__init__.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2021/11/2 11:16
 # @Author  : ZhaoXiangPeng
-# @File    : __init__.py.py
+# @File    : __init__.py
+
+
+class ItemDo:
+    pipeline: str = None
+
+
+class RequestDo:
+    pass
--- ReSpider/core/buffer/item_buffer.py
+++ ReSpider/core/buffer/item_buffer.py
@@ -3,6 +3,32 @@
 # @Author  : ZhaoXiangPeng
 # @File    : item_buffer.py
 
+import asyncio
+from . import ItemDo
+
+MAX_ITEM_COUNT = 500
+
 
 class ItemBuffer:
-    pass
+
+    __BUFFER__ = {
+        # MysqlPipeline
+        # MongodbPipeline
+        # ...
+    }
+
+    def add_buffer(self, data: ItemDo):
+        pipeline_key = data.pipeline
+        if self.__BUFFER__.get(pipeline_key) is None:
+            self.__BUFFER__[pipeline_key]: list = []  # 暂时先用list结构存储
+        self.__BUFFER__[pipeline_key].append(data)
+
+    def save_data(self):
+        pass
+
+    def check_count(self):
+        for pipeline, items in self.__BUFFER__.items():
+            if items.__len__() > MAX_ITEM_COUNT:
+                # do something
+                # ...
+                self.__BUFFER__[pipeline] = []
