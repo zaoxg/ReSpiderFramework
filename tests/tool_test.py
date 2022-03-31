d12979f8b018151a09c9b4d1b540a5d8c8dab902
zhaoxiangpeng <1943364377@qq.com>
2022-03-31 17:36:59 +0800

    删除垃圾代码

--- tests/tool_test.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2022/3/28 10:00
-# @Author  : ZhaoXiangPeng
-# @File    : tool_test.py
-
-import ReSpider.utils.tools as tools
-
-print(tools.make_sql_from_('website', {'name': 'runoob', 'url': 'www.runoob.com'}))
